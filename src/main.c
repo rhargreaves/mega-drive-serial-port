c4d56851729e3c2334ba7cefb0cb5f23165ce523
rhargreaves <robert@roberthargreaves.net>
2019-11-09 00:08:59 +0000

    Refactor to constants

--- src/main.c
+++ src/main.c
@@ -9,17 +9,23 @@
 #define VDP_MODE_REG_3 0xB
 #define VDP_IE2 0x08
 
-#define SCTRL_300_BPS 0xF0
-#define SCTRL_1200_BPS 0xB0
-#define SCTRL_2400_BPS 0x70
-#define SCTRL_4800_BPS 0x30
+#define CTRL_PCS_OUT 0x7F
+
+#define SCTRL_SIN 0x20
+#define SCTRL_SOUT 0x10
+#define SCTRL_300_BPS 0xC0
+#define SCTRL_1200_BPS 0x80
+#define SCTRL_2400_BPS 0x40
+#define SCTRL_4800_BPS 0x00
 
 #define SCTRL_RRDY 0x2
 #define SCTRL_RINT 0x8
 
+#define INT_MASK_LEVEL_ENABLE_ALL 1
+
 #define BUFFER_LEN 2048
 
-const u16 min_y = 10;
+const u16 min_y = 11;
 const u16 max_x = 39;
 const u16 max_lines = 5;
 
@@ -55,48 +61,6 @@ static void set_ctrl(u16 value)
 
 static char buffer[BUFFER_LEN];
 
-static void ext_int_callback(void)
-{
-    buffer[write_head++] = read();
-    ui_dirty = TRUE;
-}
-
-void serial_init(void)
-{
-    set_sctrl(SCTRL_4800_BPS | SCTRL_RINT);
-    set_ctrl(0x7F); // TH-Int:OFF, PC6..PC0: OUTPUT.
- //   VDP_setReg(
-  //      VDP_MODE_REG_3, VDP_IE2); // Enable IE2 (enable external interrupts)
-    SYS_setExtIntCallback(&ext_int_callback);
-    SYS_setInterruptMaskLevel(1);
-}
-
-void print_sctrl(void)
-{
-    vs8* pb;
-    pb = (s8*)PORT2_SCTRL;
-    s8 data = *pb;
-
-    for (u16 i = 0; i < 8; i++) {
-        char text[2];
-        sprintf(text, "%d", GET_BIT(data, 7 - i));
-        VDP_drawText(text, i * 5, 3);
-    }
-}
-
-void print_ctrl(void)
-{
-    vs8* pb;
-    pb = (s8*)PORT2_CTRL;
-    s8 data = *pb;
-
-    for (u16 i = 0; i < 8; i++) {
-        char text[2];
-        sprintf(text, "%d", GET_BIT(data, 7 - i));
-        VDP_drawText(text, i * 5, 6);
-    }
-}
-
 u8 can_read(void)
 {
     vs8* pb = (s8*)PORT2_SCTRL;
@@ -109,19 +73,65 @@ u8 read(void)
     return *pb;
 }
 
-void _outbyte(u8 c)
+static u8 can_read_buffer(void)
 {
-    vs8* pb = (s8*)PORT2_SCTRL;
-    u8 byte;
-    byte = c;
+    return write_head != read_head;
+}
 
-    /* wait for TFUL low */
-    while (!(*pb & 0x02)) {
-        pb = (s8*)PORT2_SCTRL;
+static u8 read_buffer(void)
+{
+    u8 data = buffer[read_head++];
+    if (read_head == BUFFER_LEN) {
+        read_head = 0;
     }
+    return data;
+}
 
-    pb = (s8*)PORT2_TX;
-    *pb = byte;
+static void write_buffer(u8 data)
+{
+    buffer[write_head++] = data;
+    if (write_head == BUFFER_LEN) {
+        write_head = 0;
+    }
+}
+
+static void ext_int_callback(void)
+{
+    write_buffer(read());
+    ui_dirty = TRUE;
+}
+
+void serial_init(void)
+{
+    set_sctrl(SCTRL_4800_BPS | SCTRL_RINT | SCTRL_SIN | SCTRL_SOUT);
+    set_ctrl(CTRL_PCS_OUT);
+    VDP_setReg(VDP_MODE_REG_3, VDP_IE2);
+    SYS_setExtIntCallback(&ext_int_callback);
+    SYS_setInterruptMaskLevel(INT_MASK_LEVEL_ENABLE_ALL);
+}
+
+void print_sctrl(void)
+{
+    vs8* pb;
+    pb = (s8*)PORT2_SCTRL;
+    s8 data = *pb;
+    for (u16 i = 0; i < 8; i++) {
+        char text[2];
+        sprintf(text, "%d", GET_BIT(data, 7 - i));
+        VDP_drawText(text, i * 5, 3);
+    }
+}
+
+void print_ctrl(void)
+{
+    vs8* pb;
+    pb = (s8*)PORT2_CTRL;
+    s8 data = *pb;
+    for (u16 i = 0; i < 8; i++) {
+        char text[2];
+        sprintf(text, "%d", GET_BIT(data, 7 - i));
+        VDP_drawText(text, i * 5, 6);
+    }
 }
 
 static void increment_cursor(Cursor* cur)
@@ -147,14 +157,15 @@ static void read_direct(Cursor* cur)
 
 static void read_from_buffer(Cursor* cur)
 {
-    if (write_head > read_head) {
-        u8 data = buffer[read_head++];
+    if (can_read_buffer()) {
+        u8 data = read_buffer();
         char buf[2] = { (char)data, 0 };
         VDP_drawText(buf, cur->x, cur->y + min_y);
         increment_cursor(cur);
         if (cur->x == 0 && cur->y == 0) {
             VDP_clearTextArea(0, min_y, max_x + 1, max_lines + 1);
         }
+        ui_dirty = TRUE;
     }
 }
 
@@ -165,19 +176,33 @@ int main()
     VDP_drawText("Mega Drive Serial Port Diagnostics", 3, 0);
     VDP_drawText("BPS1 BPS0 SIN  SOUT RINT RERR RRDY TFUL", 0, 2);
     VDP_drawText("INT  PC6  PC5  PC4  PC3  PC2  PC1  PC0", 0, 5);
-    VDP_drawText("Recv:", 0, 9);
+    VDP_drawText("Read Buffer:", 0, 10);
+
+    print_ctrl();
 
     Cursor cur = { 0, 0 };
     while (TRUE) {
         print_sctrl();
-        print_ctrl();
-        read_direct(&cur);
+        //  read_direct(&cur);
         read_from_buffer(&cur);
 
-        if(ui_dirty) {
-            char text[5];
-            sprintf(text, "%d", BUFFER_LEN - write_head);
-            VDP_drawText(text, 0, 24);
+        if (ui_dirty) {
+            /*
+            ----R--------W-----
+            xxxxx        xxxxxx
+
+            ----W--------R-----
+                xxxxxxxxx
+            */
+            u16 bufferLeft;
+            if (write_head >= read_head) {
+                bufferLeft = BUFFER_LEN - (write_head - read_head);
+            } else {
+                bufferLeft = read_head - write_head;
+            }
+            char text[32];
+            sprintf(text, "Bytes Free: %-4d", bufferLeft);
+            VDP_drawText(text, 0, 25);
             ui_dirty = FALSE;
         }
     }
